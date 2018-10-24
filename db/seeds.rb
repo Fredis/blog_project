a07508cefe3fa23d18421ffdbe3d0bde7537bccb
Fredis <fredericstojicevic@gmail.com>
2018-10-24 15:21:23 +0200

    Creation of some associations...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do |index|
+	article = Article.create!(user_id: 2, title: Faker::StarWars.specie, content: Faker::StarWars.quote)
+end
